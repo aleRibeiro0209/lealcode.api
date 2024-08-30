7567129c55906b878acb8169179fcb69377dea94
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-08-30 17:56:19 -0300

    Update - index

--- public/index.php
+++ public/index.php
@@ -9,4 +9,4 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-new Routes();
+new Routes;
