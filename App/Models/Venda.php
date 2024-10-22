65b79fb77396d28c632ae647ddf60b9c3bf415b2
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-10-21 23:54:07 -0300

    Update - Finalizando o Cadastro de vendas

--- App/Models/Venda.php
+++ App/Models/Venda.php
@@ -17,6 +17,13 @@ class Venda {
     private float $jurosAplicados;
     private ?string $instituicaoFinanceira;
     private float $taxasEncargosAdicionais;
+    private ?string $statusDoc;
+    private ?string $dataPrevisao;
+    private ?string $responsavel;
+    private ?string $seguradora;
+    private ?string $tiposGarantia;
+    private ?string $servicosExtras;
+    private ?float $valorSeguro;
     private ?string $observacoes;
     private int $idFuncionario;
     private int $idVeiculo;
@@ -33,6 +40,13 @@ class Venda {
         $this->jurosAplicados = isset($data->jurosAplicados) ? $data->jurosAplicados : 0;
         $this->instituicaoFinanceira = isset($data->instituicaoFinanceira) ? $data->instituicaoFinanceira : null;
         $this->taxasEncargosAdicionais = isset($data->taxasEncargosAdicionais) ? $data->taxasEncargosAdicionais : 0;
+        $this->statusDoc = isset($data->statusDoc) ? $data->statusDoc : null;
+        $this->dataPrevisao = isset($data->dataPrevisao) ? $data->dataPrevisao : null;
+        $this->responsavel = isset($data->responsavel) ? $data->responsavel : null;
+        $this->seguradora = isset($data->seguradora) ? $data->seguradora : null;
+        $this->tiposGarantia = isset($data->tiposGarantia) ? $data->tiposGarantia : null;
+        $this->servicosExtras = isset($data->servicosExtras) ? $data->servicosExtras : null;
+        $this->valorSeguro = isset($data->valorSeguro) ? $data->valorSeguro : null;
         $this->observacoes = isset($data->observacoes) ? $data->observacoes : null;
         $this->idFuncionario = $data->idFuncionario;
         $this->idVeiculo = $data->idVeiculo;
@@ -67,7 +81,7 @@ class Venda {
     public function create($data) {
         $this->contructVenda($data);
 
-        $sql = "INSERT INTO tbVendas (dataVenda, formaPagamento, valorTotal, descontoAplicado, valorEntrada, quantidadeParcelas, valorParcela, jurosAplicados, instituicaoFinanceira, taxasEncargosAdicionais, observacoes, idFuncionario, idVeiculo, idCliente) VALUES (:dataVenda, :formaPagamento, :valorTotal, :descontoAplicado, :valorEntrada, :quantidadeParcelas, :valorParcela, :jurosAplicados, :instituicaoFinanceira, :taxasEncargosAdicionais, :observacoes, :idFuncionario, :idVeiculo, :idCliente)";
+        $sql = "INSERT INTO tbVendas (dataVenda, formaPagamento, valorTotal, descontoAplicado, valorEntrada, quantidadeParcelas, valorParcela, jurosAplicados, instituicaoFinanceira, taxasEncargosAdicionais, statusDoc, dataPrevisao, responsavel, seguradora, tiposGarantia, servicosExtras, valorSeguro, observacoes, idFuncionario, idVeiculo, idCliente) VALUES (:dataVenda, :formaPagamento, :valorTotal, :descontoAplicado, :valorEntrada, :quantidadeParcelas, :valorParcela, :jurosAplicados, :instituicaoFinanceira, :taxasEncargosAdicionais, :statusDoc, :dataPrevisao, :responsavel, :seguradora, :tiposGarantia, :servicosExtras, :valorSeguro, :observacoes, :idFuncionario, :idVeiculo, :idCliente)";
 
 
         try {
@@ -82,6 +96,13 @@ class Venda {
             $stmt->bindParam(':jurosAplicados', $this->jurosAplicados);
             $stmt->bindParam(':instituicaoFinanceira', $this->instituicaoFinanceira);
             $stmt->bindParam(':taxasEncargosAdicionais', $this->taxasEncargosAdicionais);
+            $stmt->bindParam(':statusDoc', $this->statusDoc);
+            $stmt->bindParam(':dataPrevisao', $this->dataPrevisao);
+            $stmt->bindParam(':responsavel', $this->responsavel);
+            $stmt->bindParam(':seguradora', $this->seguradora);
+            $stmt->bindParam(':tiposGarantia', $this->tiposGarantia);
+            $stmt->bindParam(':servicosExtras', $this->servicosExtras);
+            $stmt->bindParam(':valorSeguro', $this->valorSeguro);
             $stmt->bindParam(':observacoes', $this->observacoes);
             $stmt->bindParam(':idFuncionario', $this->idFuncionario);
             $stmt->bindParam(':idVeiculo', $this->idVeiculo);
@@ -99,8 +120,29 @@ class Venda {
     }
 
     public function update($id, $data) {
-        $sql = "UPDATE tbVendas SET dataVenda = :dataVenda, formaPagamento = :formaPagamento, valorTotal = :valorTotal, descontoAplicado = :descontoAplicado, valorEntrada = :valorEntrada, quantidadeParcelas = :quantidadeParcelas, valorParcela = :valorParcela, jurosAplicados = :jurosAplicados, instituicaoFinanceira = :instituicaoFinanceira, taxasEncargosAdicionais = :taxasEncargosAdicionais, observacoes = :observacoes, idFuncionario = :idFuncionario, idVeiculo = :idVeiculo, idCliente = :idCliente WHERE idVenda = :id";
-
+        $sql = "UPDATE tbVendas SET 
+            dataVenda = :dataVenda, 
+            formaPagamento = :formaPagamento, 
+            valorTotal = :valorTotal, 
+            descontoAplicado = :descontoAplicado, 
+            valorEntrada = :valorEntrada, 
+            quantidadeParcelas = :quantidadeParcelas, 
+            valorParcela = :valorParcela, 
+            jurosAplicados = :jurosAplicados, 
+            instituicaoFinanceira = :instituicaoFinanceira, 
+            taxasEncargosAdicionais = :taxasEncargosAdicionais, 
+            statusDoc = :statusDoc, 
+            dataPrevisao = :dataPrevisao, 
+            responsavel = :responsavel, 
+            seguradora = :seguradora, 
+            tiposGarantia = :tiposGarantia, 
+            servicosExtras = :servicosExtras, 
+            valorSeguro = :valorSeguro, 
+            observacoes = :observacoes, 
+            idFuncionario = :idFuncionario, 
+            idVeiculo = :idVeiculo, 
+            idCliente = :idCliente 
+        WHERE idVenda = :id";
 
         try {
             $stmt = Model::getConn()->prepare($sql);
@@ -114,6 +156,13 @@ class Venda {
             $stmt->bindParam(':jurosAplicados', $data->jurosAplicados);
             $stmt->bindParam(':instituicaoFinanceira', $data->instituicaoFinanceira);
             $stmt->bindParam(':taxasEncargosAdicionais', $data->taxasEncargosAdicionais);
+            $stmt->bindParam(':statusDoc', $data->statusDoc);
+            $stmt->bindParam(':dataPrevisao', $data->dataPrevisao);
+            $stmt->bindParam(':responsavel', $data->responsavel);
+            $stmt->bindParam(':seguradora', $data->seguradora);
+            $stmt->bindParam(':tiposGarantia', $data->tiposGarantia);
+            $stmt->bindParam(':servicosExtras', $data->servicosExtras);
+            $stmt->bindParam(':valorSeguro', $data->valorSeguro);
             $stmt->bindParam(':observacoes', $data->observacoes);
             $stmt->bindParam(':idFuncionario', $data->idFuncionario);
             $stmt->bindParam(':idVeiculo', $data->idVeiculo);
